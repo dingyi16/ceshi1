940015eb3e0f849ee4aaa9d411cf300d07171105
dingyi16 <dzssjhh@163.com>
2019-07-31 00:22:40 +0800

    #测试代码

---
+++ ceshi.java
@@ -0,0 +1,9 @@
+
+public class ceshi {
+    public static void main(String[] args) {
+        double a = 0.7, b = 0.4;
+        float a1 = 0.7f, b1 = 0.4f;
+        System.out.println(Math.round(a) + " " + Math.round(b));
+        System.out.println(Math.round(a1) + " " + Math.round(b1));
+    }
+}
